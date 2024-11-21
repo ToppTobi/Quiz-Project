d70df197f6f0554fffb5366dabc5fbd176d210e2
ToppTobi <tobias.topp@gmx.ch>
2024-11-21 09:54:30 +0100

    Tried to implement a card switch feature

--- src/components/create-set/create-set.component.ts
+++ src/components/create-set/create-set.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-create-set',
   standalone: true,
-  imports: [],
-  templateUrl: './create-set.component.html',
-  styleUrl: './create-set.component.css'
+  imports: [
+    NgClass
+  ],
+  templateUrl: 'create-set.component.html',
+  styleUrls: ['create-set.component.css']
 })
 export class CreateSetComponent {
 
+  isRotated: boolean = false;
+
+  isClicked() {
+    console.log("Das ist ein Test!");
+  }
+
+  rotateCard() {
+    this.isRotated = !this.isRotated; // Schaltet zwischen true und false
+    console.log('Card rotated:', this.isRotated); // Debug-Ausgabe
+  }
 }
