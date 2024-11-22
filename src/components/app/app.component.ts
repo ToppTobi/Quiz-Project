50014102c4ab9cfd0b7704d53a7b27528440bb3e
ToppTobi <tobias.topp@gmx.ch>
2024-11-22 13:41:21 +0100

    Added a Icon to the nav bar

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {NgForOf, NgIf} from "@angular/common";
+import {NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {RouterLink, RouterOutlet} from "@angular/router";
 
@@ -7,7 +7,7 @@ import {RouterLink, RouterOutlet} from "@angular/router";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgForOf, NgIf, FormsModule, RouterOutlet, RouterLink],
+  imports: [NgForOf, NgIf, FormsModule, RouterOutlet, RouterLink, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 
